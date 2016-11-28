b3a66293d6b270668391b4efe2c7f55a36049c98
Robin Ricard <ricard.robin@gmail.com>
2016-11-28 02:54:53 +0100

    Tested store

--- src/store.ml
+++ src/store.ml
@@ -1,35 +1,30 @@
 type listener = unit -> unit;;
 
 type ('stateType, 'actionType) store = {
-  state: 'stateType option ref;
-  listeners: listener list ref;
-  get_state: unit -> 'stateType option;
+  get_state: unit -> 'stateType;
   dispatch: 'actionType -> unit;
   subscribe: listener -> listener;
 };;
 
-let createStore reducer =
-  let state = ref None
-  and listeners = ref [] in
-  {
-    state;
-    listeners;
-    get_state=(fun () -> !state);
-    dispatch=(fun action -> (
-      (* Deref the state, execute the reducer with it, re-assign it *)
-      state := reducer action !state;
-      (* Call each listener *)
-      List.map (fun l -> l ()) !listeners;
+let createStore reducer initialState =
+  let state = ref initialState in
+  let listeners = ref [] in
+  let get_state = (fun () -> !state)
+  and dispatch = (fun action ->
+    (* Deref the state, execute the reducer with it, re-assign it *)
+    state := reducer !state action;
+    (* Call each listener *)
+    List.map (fun l -> l ()) !listeners;
+    ();
+  )
+  and subscribe = (fun listener ->
+    (* Cons the listener to the list *)
+    listeners := listener :: !listeners;
+    (* Return a function to unsubscribe from the listener *)
+    (fun () ->
+      (* Filter out the listener to unsubscribe *)
+      listeners := List.filter (fun l -> l != listener) !listeners;
       ();
-    ));
-    subscribe=(fun listener ->
-      (* Cons the listener to the list *)
-      listeners := listener :: !listeners;
-      (* Return a function to unsubscribe from the listener *)
-      (fun () ->
-        (* Filter out the listener to unsubscribe *)
-        listeners := List.filter (fun l -> l != listener) !listeners;
-        ();
-      );
     );
-  };;
+  ) in
+  { get_state; dispatch; subscribe; };;
--- test/storeTest.ml
+++ test/storeTest.ml
@@ -1,3 +1,52 @@
 open OUnit2;;
+open Store;;
 
-let storeSuite = [];
+type operation_action =
+  | Add of float
+  | Multiply of float;;
+
+let reducer state action = 
+  match action with
+  | Add x -> state +. x
+  | Multiply x -> state *. x;;
+
+let storeCreationTest test_ctxt =
+  let store = createStore reducer 0.0 in
+  assert_equal (store.get_state ()) 0.0;;
+
+let storeDispatchAndGetTest test_ctxt =
+  let store = createStore reducer 0.0 in
+  store.dispatch (Add 1.0);
+  assert_equal (store.get_state ()) 1.0;
+  store.dispatch (Add 1.0);
+  store.dispatch (Multiply (-2.0));
+  assert_equal (store.get_state ()) (-4.0);;
+
+let storeListeners test_ctxt =
+  let listener1_called = ref false
+  and listener2_called = ref false
+  and listener3_called = ref false
+  and store = createStore reducer 0.0 in
+  store.subscribe (fun () -> 
+    listener1_called := true;
+    ();
+  );
+  store.subscribe (fun () -> 
+    listener2_called := true;
+    ();
+  );
+  store.subscribe (fun () -> 
+    listener3_called := true;
+    ();
+  ) ();
+  store.dispatch (Add 1.0);
+  assert_equal !listener1_called true;
+  assert_equal !listener2_called true;
+  assert_equal !listener3_called false;
+  ;;
+
+let storeSuite = [
+  "creation">:: storeCreationTest;
+  "dispatch">:: storeDispatchAndGetTest;
+  "listeners">:: storeListeners;
+];
